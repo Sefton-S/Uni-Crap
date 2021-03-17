9c7fa8e31cc3b7cf2b831f135a148a06e63a2e7b
Sefton-S <sefton.shillingford@gmail.com>
2021-03-17 19:02:30 +0000

    msg

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            while(loop == true)
+            {
+            TicketMachine t = new TicketMachine();
+            Console.WriteLine("What is the price of the ticket?");
+            t.ticketPrice = Convert.ToInt32(Console.ReadLine());
+            //Console.ReadLine();
+            //int amount = 5;
+            Console.WriteLine("How much money do you want to enter into the machine?");
+            t.insertMoney(Convert.ToInt32(Console.ReadLine()));
+            t.printTicket();
+
+            }
+            
+        }
+    }
+}
---
+++ TicketMachine.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M
+{
+    //class TicketMachine models a simple ticket machine
+    //It prints a ticket of a single price if there is enough money
+    class TicketMachine
+    {
+        //fields and properties
+        private int _ticketPrice;
+        private int _balance;
+        private int _total;
+
+        public int ticketPrice
+        {
+            get { return _ticketPrice; }
+            set
+            {
+                if (value > 0)
+                {
+                    _ticketPrice = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Fucking idiot lol");
+                }
+                    
+                
+                
+            }
+        }
+
+        public int balance
+        {
+            get { return _balance; }
+            private set { _balance = value; }
+        }
+
+        public int total
+        {
+            get { return _total; }
+            private set { _total = value; }
+        }
+
+        //methods
+        //insertMoney - receive an amount of money from the customer
+        //parameter: int
+        //return: void
+        public void insertMoney(int amount)
+        {
+            
+            if(amount > 0)
+            {
+                _balance += amount;
+                Console.WriteLine("stuff");
+            }
+            else
+            {
+                Console.WriteLine("Amount must be greater than 0");
+            }
+
+        }
+
+        //printTicket - simulate the output of a ticket
+        //parameter: none
+        //return: void
+        public void printTicket()
+        {
+            if(_ticketPrice > _balance)
+            {
+                Console.WriteLine($"You do not have enough money to buy the ticket. \nTicket price: £{_ticketPrice}\nYour balance: £{_balance}");
+            }
+
+            else
+            {
+                _balance -= _ticketPrice;
+                _total += _ticketPrice;
+                Console.WriteLine("******************");
+                Console.WriteLine(" SoCS Line Ticket");
+                Console.WriteLine($"   Price = £{_ticketPrice}");
+                Console.WriteLine("******************");
+                status();
+            }
+        }
+
+        //status - returns the current total of money in this machine
+        //parameter: none
+        //return: void
+        public void status()
+        {
+             Console.WriteLine($"Balance: £{_balance}"); Console.WriteLine($"Total: £{_total}"); Console.WriteLine($"Ticket Price: £{_ticketPrice}");
+        }
+
+
+    }
+}
